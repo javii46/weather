a489588f15cb8513023b9281d75585d1bf98a558
javii46 <javi_14_dh@hotmail.com>
2014-02-12 10:25:42 +0100

    Creacion hasta diccionario

---
+++ weather.py
@@ -0,0 +1,17 @@
+#encoding=utf-8
+import json
+import requests
+print 'Lista de las ciudades.' """
+
+1. Almeria
+2. Cadiz
+3. Cordoba
+4. Granada
+5. Huelva
+6. Jaen
+7. Malaga
+8. Sevilla
+
+"""
+
+diccionario = {1:"Almeria",2:"Cadiz",3:"Cordoba",4:"Granada",5:"Huelva",6:"Jaen",7:"Malaga",8:"Sevilla"}
