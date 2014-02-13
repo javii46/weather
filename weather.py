8ff00ae5c14aee260ff536e45ba459f48e34de84
javii46 <javi_14_dh@hotmail.com>
2014-02-13 08:32:14 +0100

    weather completo condiccionario definido como cadena

--- weather.py
+++ weather.py
@@ -14,14 +14,12 @@ print 'Lista de las ciudades.' """
 
 """
 
-diccionario = {1:"Almeria",2:"Cadiz",3:"Cordoba",4:"Granada",5:"Huelva",6:"Jaen",7:"Malaga",8:"Sevilla"}
+diccionario = {"1":"Almeria","2":"Cadiz","3":"Cordoba","4":"Granada","5":"Huelva","6":"Jaen","7":"Malaga","8":"Sevilla"}
 
-consulta = int(raw_input("Introduce el número de la ciudad a consultar: "))
+consulta = raw_input("Introduce el número de la ciudad para saber su temperatura: ")
 prov=diccionario[consulta]
 respuesta=requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':'%s,%s' % (prov, "spain")})
-
-
 dicc=json.loads(respuesta.text)
-temperatura=dicc["main"]["temp"]
-gradoscentigrados=temperatura
+temperatura=dicc["main"]["temp"] 
+gradoscentigrados=temperatura - 273
 print "La temperatura de",prov,"es",gradoscentigrados,"grados centígrados."
